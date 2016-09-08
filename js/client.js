93bc2c57018f48105e1d03148a6803c453b13e84
Kevin Simper <kevin.simper@gmail.com>
2016-09-08 23:59:07 +0200

    Organise into classes

--- js/client.js
+++ js/client.js
@@ -1,69 +1,93 @@
-document.querySelector('#file').addEventListener('change', (e) => {
-  let file = e.target.files[0]
-  getImageDetails(file, (image) => {
-    let { width, height } = image
-    document.querySelector('#image').appendChild(image)
-    let context = createCanvasContext(width, height)
-    context.drawImage(image, 0, 0)
-    let { gridWidth, gridHeight} = calculateGrid(width, height)
-    console.log(gridWidth, gridHeight)
-    generateMosaic(context, gridWidth, gridHeight)
-  })
-})
-
-function generateMosaic(context, gridWidth, gridHeight) {
-  let rowsLoading = []
-  for(let y = 0; y < gridHeight; y++) {
-    let div = document.createElement('div')
-    div.className = 'row'
-    div.style.display = 'none'
-    let images = []
-    for(let x = 0; x < gridWidth; x++) {
-      let data = context.getImageData(x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT).data
-      let block = generateBlock(data)
-      images.push(imageLoadedPromise(block))
-      div.appendChild(block)
-    }
-    document.querySelector('#mosaic').appendChild(div)
-    rowsLoading.push(Promise.all(images).then(function () {
-      return [div, y]
-    }))
+class MosaicApp {
+  constructor() {
+    document.querySelector('#file').addEventListener('change', this.fileChange.bind(this))
   }
-  // run the promise one at a time left to right
-  rowsLoading.reduce((previous, currentValue) => {
-    // wait for the previous to finish
-    return previous.then(() => {
-      // then wait for the current one
-      return currentValue.then((result) => {
-        rowLoaded(result[0], result[1])
-      })
+  fileChange(e) {
+    let file = e.target.files[0]
+    let image = new ImageLoader(file)
+    image.getDetails(file, (image) => {
+      let { width, height } = image
+      document.querySelector('#image').appendChild(image)
+      let context = createCanvasContext(width, height)
+      context.drawImage(image, 0, 0)
+      let { gridWidth, gridHeight} = this.calculateGrid(width, height)
+      console.log(gridWidth, gridHeight)
+      this.generate(context, gridWidth, gridHeight)
     })
-  }, Promise.resolve())
-}
-
-function rowLoaded (div, y) {
-  console.log(y)
-  div.style.display = ''
-}
-
-function imageLoadedPromise (image) {
-  return new Promise((resolve, reject) => {
-    image.onload = () => {
-      resolve()
-    }
-  })
-}
-
-function getImageDetails (file, callback) {
-  let image = document.createElement('img')
-  image.onload = function () {
-    return callback.bind(this, image)
-  }();
-  let reader = new FileReader()
-  reader.onload = () => {
-    image.src = reader.result
   }
-  reader.readAsDataURL(file)
+  generate(context, gridWidth, gridHeight) {
+    let rowsLoading = []
+    for(let y = 0; y < gridHeight; y++) {
+      let div = document.createElement('div')
+      div.className = 'row'
+      div.style.display = 'none'
+      let images = []
+      for(let x = 0; x < gridWidth; x++) {
+        let data = context.getImageData(x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT).data
+        let block = this.generateBlock(data)
+        images.push(this.makeImageLoadedPromise(block))
+        div.appendChild(block)
+      }
+      document.querySelector('#mosaic').appendChild(div)
+      rowsLoading.push(Promise.all(images).then(function () {
+        return [div, y]
+      }))
+    }
+    this.sequentialLoad(rowsLoading)
+    // run the promise one at a time left to right
+  }
+  sequentialLoad(rows) {
+    rows.reduce((previous, currentValue) => {
+      // wait for the previous to finish
+      return previous.then(() => {
+        // then wait for the current one
+        return currentValue.then((result) => {
+          this.rowLoaded(result[0], result[1])
+        })
+      })
+    }, Promise.resolve())
+  }
+  rowLoaded (div, y) {
+    console.log(y)
+    div.style.display = ''
+  }
+  makeImageLoadedPromise (image) {
+    return new Promise((resolve, reject) => {
+      image.onload = () => {
+        resolve()
+      }
+    })
+  }
+  calculateGrid (imageWidth, imageHeight) {
+    return {
+      gridWidth: Math.ceil(imageWidth / TILE_WIDTH),
+      gridHeight: Math.ceil(imageHeight / TILE_HEIGHT)
+    }
+  }
+  generateBlock (data) {
+    let rgbArray = calculateAverageColor(data)
+    const hex = convertToHex(...rgbArray)
+    return createColorBlock(hex)
+  }
+
+}
+new MosaicApp()
+
+class ImageLoader {
+  constructor() {
+    this.image = document.createElement('img')
+  }
+  getDetails (file, callback) {
+    console.log(this)
+    this.image.onload = function () {
+      return callback.bind(this, this.image)
+    }.bind(this)();
+    let reader = new FileReader()
+    reader.onload = () => {
+      this.image.src = reader.result
+    }
+    reader.readAsDataURL(file)
+  }
 }
 
 function createCanvasContext (width, height) {
@@ -100,17 +124,4 @@ function calculateAverageColor (data) {
   g = Math.floor(Math.sqrt(g / pixels))
   b = Math.floor(Math.sqrt(b / pixels))
   return [r, g, b]
-}
-
-function calculateGrid (imageWidth, imageHeight) {
-  return {
-    gridWidth: Math.ceil(imageWidth / TILE_WIDTH),
-    gridHeight: Math.ceil(imageHeight / TILE_HEIGHT)
-  }
-}
-
-function generateBlock (data) {
-  let rgbArray = calculateAverageColor(data)
-  const hex = convertToHex(...rgbArray)
-  return createColorBlock(hex)
 }
