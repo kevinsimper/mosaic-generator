5c817c202779553738a109078d00067167097954
Kevin Simper <kevin.simper@gmail.com>
2016-09-08 18:54:56 +0200

    Load the image and get the width and height

--- js/client.js
+++ js/client.js
@@ -1,3 +1,15 @@
 document.addEventListener('change', (e) => {
-  console.log(e.target.files[0])
+  let file = e.target.files[0]
+  let canvas = document.createElement('canvas')
+  let content = canvas.getContext('2d')
+  let image = document.createElement('img')
+  image.onload = () => {
+    console.log(image.width, image.height)
+    document.body.appendChild(image)
+  }
+  let reader = new FileReader()
+  reader.onload = () => {
+    image.src = reader.result
+  }
+  reader.readAsDataURL(file)
 })
