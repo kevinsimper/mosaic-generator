0a2dbb0fbd577bea9c85b1294c6294136ff2f520
Kevin Simper <kevin.simper@gmail.com>
2016-09-08 18:58:43 +0200

    Draw the image to a canvas

--- js/client.js
+++ js/client.js
@@ -1,11 +1,14 @@
 document.addEventListener('change', (e) => {
   let file = e.target.files[0]
   let canvas = document.createElement('canvas')
-  let content = canvas.getContext('2d')
+  let context = canvas.getContext('2d')
   let image = document.createElement('img')
   image.onload = () => {
-    console.log(image.width, image.height)
-    document.body.appendChild(image)
+    let { width, height } = image
+    canvas.height = height
+    canvas.width = width
+    context.drawImage(image, 0, 0)
+    document.body.appendChild(canvas)
   }
   let reader = new FileReader()
   reader.onload = () => {
